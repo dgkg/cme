7d1edca66de2bcc84204a716ca13ba076435be36
Henri Lepic <henrilepic@gmail.com>
2014-04-23 18:28:48 +0200

    déplacement d'éléments du modèle

--- app/actionForum.go
+++ app/actionForum.go
@@ -1,11 +1,52 @@
 package app
 
 import (
-	//_ "github.com/go-sql-driver/mysql"
-	//"github.com/jinzhu/gorm"
 	. "strconv"
 	"strings"
+	"time"
 )
+
+type Forum struct {
+	Id              int64
+	UserId          int64
+	ForumCategoryId int64
+	Title           string `sql:"type:varchar(100);"`
+	Text            string
+	Keywords        string `sql:"type:varchar(200);"`
+	IsSolved        int64
+	IsOnline        int64
+	PostNumb        int64  `sql:"-"` // Ignore this field
+	CategoryTitle   string `sql:"-"` // Ignore this field
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+	Posts           []ForumPost
+}
+
+type ForumCategory struct {
+	Id        int64
+	Title     string `sql:"type:varchar(100);"`
+	Url       string `sql:"_"` // Ignore this field
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type ForumPost struct {
+	Id        int64
+	ForumId   int64
+	UserId    int64
+	Text      string
+	IsOline   int64
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+/*
+// Helper de vue
+type ForumViewHelper struct {
+	CategoryTitle string
+	Forum
+}
+*/
 
 // fonction public
 // permet d'enregistrer les éléments du formulaire
