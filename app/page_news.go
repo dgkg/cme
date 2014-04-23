ab2b42780f1479a4a2f6072d30e8846ee60a86a1
Henri Lepic <henrilepic@gmail.com>
2014-04-23 18:59:09 +0200

    suppression des fichiers modèles et renomage

--- app/page_news.go
+++ app/page_news.go
@@ -4,7 +4,44 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	"log"
+	"time"
 )
+
+type News struct {
+	Id             int64
+	UserId         int64
+	NewsCategoryId int64
+	Title          string `sql:"type:varchar(100);"`
+	Text           string
+	Keywords       string `sql:"type:varchar(200);"`
+	IsOnline       int64
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	Posts          []NewsPost
+}
+
+type NewsCategory struct {
+	Id        int64
+	Title     string `sql:"type:varchar(100);"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type NewsPost struct {
+	Id        int64
+	NewsId    int64
+	UserId    int64
+	Text      string
+	IsOline   int64
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Helper de vue
+type NewsViewHelper struct {
+	CategoryTitle string
+	News
+}
 
 type PageNews struct {
 	PagesList []Paginate
