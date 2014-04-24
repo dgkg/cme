0c95d080d165183c236591965f5947d4a2a983ac
Antoine Lord <antoinelord@gmail.com>
2014-04-24 17:50:59 +0200

    Pourquoi une asso + Qui sommes nous + retouche CSS

---
+++ app/page_quisommesnous.go
@@ -0,0 +1,21 @@
+package app
+
+import (
+	"log"
+)
+
+type PageQuiSommesNous struct {
+	PageWeb
+}
+
+func (pqsn PageQuiSommesNous) View() Page {
+
+	log.Println("Qui sommes-nous? appelé")
+
+	Templ = "qui_sommes_nous"
+
+	pqsn.Title = "Qui sommes-nous?"
+	pqsn.MainClass = "pageinfo"
+
+	return pqsn
+}
