1583af7b975a6e738bc8d68f981ff287cdb89251
Antoine Lord <antoinelord@gmail.com>
2014-05-09 17:36:25 +0200

    Validation + soumission du formulaire

--- public/js/inscription.js
+++ public/js/inscription.js
@@ -11,23 +11,24 @@ $(window).load(function() {
         onValidate : function() {
 
             //Ne pas rafraîchir la page
+            $('.msg-erreur').empty();
             event.preventDefault();
         },
 
         onSuccess : function() {
 
             // Récupération des données saisies dans la page
-            var $val_insc_prenom = $('#insc-prenom').val();
-            var $val_insc_nom = $('#insc-nom').val();
-            var $val_insc_email = $('#insc-email').val();
-            var $val_insc_mdp = $('#insc-mdp').val();
+            var $val_insc_prenom = $('#insc-prenom').val().trim();
+            var $val_insc_nom = $('#insc-nom').val().trim();
+            var $val_insc_email = $('#insc-email').val().trim();
+            var $val_insc_mdp = $('#insc-mdp').val().trim();
 
             // Envois des données en AJAX après avoir été trimmé
             var posting = $.post( "/inscription/submitform", {
-                prenom : $val_insc_prenom.trim(),
-                nom : $val_insc_nom.trim(),
-                email : $val_insc_email.trim(),
-                mdp: $val_insc_mdp.trim()
+                prenom : $val_insc_prenom,
+                nom : $val_insc_nom,
+                email : $val_insc_email,
+                mdp: $val_insc_mdp
             }, function(data, status){
 
                 if (data != "error") {
