7b885be17ce174bb5988472b6e56a8709a4e5804
Antoine Lord <antoinelord@gmail.com>
2014-05-21 10:51:28 +0200

    Amélioration du CSS + Ajout du tri des élèves

--- public/js/eleves.js
+++ public/js/eleves.js
@@ -17,20 +17,4 @@ $(window).load(function() {
 		//alert(annee);
 		$('.wrapper-eleves').isotope({ filter: annee });
 	});
-
-	/* $('.eleve').click(function(e) {
-		var profilSelectionne = $(this).attr("data-userId");
-		alert(profilSelectionne);
-		window.location.href = "/antoine-lord";
-	}); */
-
-	// Effet hover sur les projets en accueil
-	/*
-	$('.etudiant-photo').contenthover({
-	    overlay_background: '#000',
-	    overlay_opacity: 0.9,
-	    data_selector: '.hover-infos',
-	});
-	*/
-
 });
